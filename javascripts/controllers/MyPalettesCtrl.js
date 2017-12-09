ad863cc998acae3344cab908680f00b619bfcaa9
Brooke Lewis <belv2c@gmail.com>
2017-12-09 00:48:11 -0600

    combined palette and color data

--- javascripts/controllers/MyPalettesCtrl.js
+++ javascripts/controllers/MyPalettesCtrl.js
@@ -1,6 +1,19 @@
 "use strict";
 
-app.controller("MyPalettesCtrl", function($scope) {
-    $scope.controller = "MyPalettesCtrl";
-    
+app.controller("MyPalettesCtrl", function($location, $rootScope, $scope, PaletteService) {
+	
+
+	const getFavoriteFbPalettes = () => {
+		PaletteService.getFavoritePalettes($rootScope.uid).then((results) => {
+			$scope.palettes = results;
+		}).catch((err) => {
+			console.log("error in getContacts", err);
+		});
+		
+	};
+
+getFavoriteFbPalettes();
+
+
+
 });
--- javascripts/controllers/ViewCtrl.js
+++ javascripts/controllers/ViewCtrl.js
@@ -5,7 +5,6 @@ app.controller("ViewCtrl", function($scope, $rootScope, ColorService, PaletteSer
 	const getThePalettes = () => {
 		PaletteService.getPalettes($rootScope.uid).then((results) => {
 			$scope.palettes = results;
-
 		}).catch((err) => {
 			console.log("error in getThePalettes", err);
 		});
@@ -23,10 +22,20 @@ app.controller("ViewCtrl", function($scope, $rootScope, ColorService, PaletteSer
 	getTheColors();
 
 
+	$scope.deletePalette = (paletteId) => {
+		PaletteService.deletePalette(paletteId).then((results) => {
+			getThePalettes();
+		}).catch((err) => {
+			console.log("error in deletePalette", err);
+		});
+	};
 
-	$scope.paletteObject = (newpalette) => {
+
+
+	$scope.paletteObject = (newpalette, newcolor) => {
 		$scope.updatedPalette = {
 			"mode": newpalette.mode,
+			"name": newcolor.name,
 			"count": newpalette.count,
 			"isFavorite": newpalette.isFavorite,
 			"uid": $rootScope.uid
